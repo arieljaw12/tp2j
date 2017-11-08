714f18107503448fd1abc0800c20342182feb321
Ariel <Ariel@DESKTOP-CUJFSJ5>
2017-11-08 12:46:46 -0300

    Empecé a hacer unas pruebas básicas y por consiguiente cree algunas clases que asumo deberían estar (Casillero, Dado, Quini6). Me trabé a la hora del manejo de mapas hash (quería agregarle a la clase Quini6 un mapa de clave jugador y valor un entero de cuantas veces puede cobrar el premio). Faltaría averiguar bien ese manejo

--- src/modelo/Jugador.java
+++ src/modelo/Jugador.java
@@ -1,8 +1,38 @@
 package modelo;
 
 public class Jugador {
+	private String nombre;
+	private int dinero;
+
+	public Jugador(String unNombre) {
+		nombre = unNombre;
+		dinero = 100000;
+	}
+
+	public Jugador() {
+		nombre = "";
+		dinero = 100000;
+	}
+
 	public int foo() {
 		return 0;
+	}
+
+	public String tuNombreEs() {
+		return nombre;
+	}
+
+	public int dineroDisponible() {
+		return dinero;
+	}
+
+	public void caerEnCasillero(Casillero unCasillero) {
+		unCasillero.afectarJugador(this);
+		
+	}
+
+	public void entregarDinero(int unMonto) {
+		dinero += unMonto;
 	}
 
 }
--- test/unitTests/JugadorTest.java
+++ test/unitTests/JugadorTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import org.junit.Assert;
 import modelo.Jugador;
+import modelo.Quini6;
 
 public class JugadorTest {
 	
@@ -13,4 +14,32 @@ public class JugadorTest {
 		Assert.assertEquals(0, jugador.foo());
 	}
 
+	@Test
+	public void testUnJugadorSeCreaConUnCiertoNombrePasadoComoParametro() {
+		Jugador jugador = new Jugador("Ariel");
+		Assert.assertEquals(jugador.tuNombreEs(), "Ariel");
+	}
+	
+	@Test
+	public void testUnJugadorSeCreaConCienMilPesosDeDineroDisponible() {
+		Jugador jugador = new Jugador("Ariel");
+		Assert.assertEquals(jugador.dineroDisponible(), 100000);
+	}
+	
+	@Test
+	public void testUnJugadorCaeEnQuini6PorPrimeraVezYSuCapitalAumentaEn50000() {
+		Jugador jugador = new Jugador("Ariel");
+		Quini6 quini6 = new Quini6();
+		jugador.caerEnCasillero(quini6);
+		Assert.assertEquals(jugador.dineroDisponible(), 150000);		
+	}
+	
+	@Test public void testUnJugadorCaeEnQuini6PorSegundaVezYSuCapitalAumentaEn30000() {
+		Jugador jugador = new Jugador("Ariel");
+		Quini6 quini6 = new Quini6();
+		quini6.registrarJugador(jugador);
+		jugador.caerEnCasillero(quini6);
+		jugador.caerEnCasillero(quini6);
+		Assert.assertEquals(jugador.dineroDisponible(), 180000);	
+	}
 }
